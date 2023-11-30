97571b287c7642eb8b9c6739ea0bf22f01cf285e
RouRai <k1215050@students.katyisd.org>
2023-11-30 08:47:13 -0600

    Revert "linted stoichiometry.py"
    This reverts commit 600c4c7d062a3d898e5905531c465fca9f82e7cc.

--- src/chemiq_noveled/operations/stoichiometry.py
+++ src/chemiq_noveled/operations/stoichiometry.py
@@ -1,12 +1,7 @@
-"""
-Offers methods to aid with Stoichiometry calculations.
-"""
-
 import numpy as np
-from numpy import ndarray
 
-from src.chemiq_noveled.base.stoichiometry import Stoichiometry
-from src.chemiq_noveled.base.balancer import Balancer
+from numpy import ndarray
+from src.chemiq_noveled.base.calculator import Calculator
 from src.chemiq_noveled.base.molecule import Molecule
 
 
@@ -14,18 +9,10 @@ def limiting_reactant_moles_coefficients(
         reactants: ndarray[str],
         coefficients: ndarray[str],
         moles: ndarray[str]):
-    """
-    Determines the limiting reactant in a reaction given coefficients, reactants, and moles of the
-    reactants.
-
-    :param reactants: The reactants in the chemical equation.
-    :param coefficients: The coefficients of the reactants in the chemical equation.
-    :param moles: Moles of the reactants in the chemical equation.
-    """
     coefficients: ndarray[int] = np.array(coefficients)
     reactants: ndarray[Molecule] = np.array([Molecule.parse(reactant) for reactant in reactants])
     moles: ndarray[float] = np.array(moles).astype(dtype=float)
-    solution = Stoichiometry.limiting_reactant_moles(
+    solution = Calculator.limiting_reactant_moles(
         reactants=reactants,
         reactant_coefficients=coefficients,
         moles=moles
@@ -42,18 +29,10 @@ def limiting_reactant_grams_coefficients(
         reactants: ndarray[str],
         coefficients: ndarray[str],
         grams: ndarray[str]):
-    """
-    Determines the limiting reactant in a reaction given coefficients, reactants, and moles of the
-    reactants.
-
-    :param reactants: The reactants in the chemical equation.
-    :param coefficients: The coefficients of the reactants in the chemical equation.
-    :param grams: Grams of the reactants in the chemical equation.
-    """
     coefficients: ndarray[int] = np.array(coefficients)
     reactants: ndarray[Molecule] = np.array([Molecule.parse(reactant) for reactant in reactants])
     grams: ndarray[float] = np.array(grams).astype(dtype=float)
-    solution = Stoichiometry.limiting_reactant_grams(
+    solution = Calculator.limiting_reactant_grams(
         reactants=reactants,
         reactant_coefficients=coefficients,
         grams=grams
@@ -62,7 +41,7 @@ def limiting_reactant_grams_coefficients(
         'reactants': str(reactants),
         'coefficients': str(coefficients),
         'grams': str(grams),
-        'moles': str(Stoichiometry.convert_grams_to_moles(reactants, grams)),
+        'moles': str(Calculator.convert_grams_to_moles(reactants, grams)),
         'solution': str(solution)
     }
 
@@ -71,18 +50,10 @@ def limiting_reactant_moles_no_coefficients(
         reactants: ndarray[str],
         products: ndarray[str],
         moles: ndarray[str]):
-    """
-    Determines the limiting reactant in a reaction given reactants, products, and moles of the
-    reactants.
-
-    :param reactants: The reactants in the chemical equation.
-    :param products: The products in the chemical equation.
-    :param moles: Moles of the reactants in the chemical equation.
-    """
     reactants: ndarray[Molecule] = np.array([Molecule.parse(reactant) for reactant in reactants])
     products: ndarray[Molecule] = np.array([Molecule.parse(product) for product in products])
     moles: ndarray[float] = np.array(moles).astype(dtype=float)
-    solution = Stoichiometry.limiting_reactant_moles_without_coefficients(
+    solution = Calculator.limiting_reactant_moles_without_coefficients(
         reactants=reactants,
         products=products,
         moles=moles
@@ -90,7 +61,7 @@ def limiting_reactant_moles_no_coefficients(
     return {
         'reactants': str(reactants),
         'products': str(products),
-        'coefficients': str(Balancer.balance_equation(reactants, products)),
+        'coefficients': str(Calculator.balance_equation(reactants, products)),
         'moles': str(moles),
         'solution': str(solution)
     }
@@ -100,18 +71,10 @@ def limiting_reactant_grams_no_coefficients(
         reactants: ndarray[str],
         products: ndarray[str],
         grams: ndarray[str]):
-    """
-    Determines the limiting reactant in a reaction given reactants, products, and grams of the
-    reactants.
-
-    :param reactants: The reactants in the chemical equation.
-    :param products: The products in the chemical equation.
-    :param grams: Grams of the reactants in the chemical equation.
-    """
     reactants: ndarray[Molecule] = np.array([Molecule.parse(reactant) for reactant in reactants])
     products: ndarray[Molecule] = np.array([Molecule.parse(product) for product in products])
     grams: ndarray[float] = np.array(grams).astype(dtype=float)
-    solution = Stoichiometry.limiting_reactant_grams_without_coefficients(
+    solution = Calculator.limiting_reactant_grams_without_coefficients(
         reactants=reactants,
         products=products,
         grams=grams
@@ -119,8 +82,8 @@ def limiting_reactant_grams_no_coefficients(
     return {
         'reactants': str(reactants),
         'product': str(products),
-        'coefficients': str(Balancer.balance_equation(reactants, products)),
+        'coefficients': str(Calculator.balance_equation(reactants, products)),
         'grams': str(grams),
-        'moles': str(Stoichiometry.convert_grams_to_moles(reactants, grams)),
+        'moles': str(Calculator.convert_grams_to_moles(reactants, grams)),
         'solution': str(solution)
     }
